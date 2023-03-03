3cb3c39aa0bf7ee3a657e3612a7712b86befa8ab
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-03-03 21:19:27 +0330

    create: email validator

--- account/models.py
+++ account/models.py
@@ -3,6 +3,7 @@ from django.core.validators import RegexValidator
 from django.core.exceptions import ValidationError
 from django.contrib.auth.models import AbstractUser
 from json import load
+from validate_email_address import validate_email
 
 # Create your models here.
 
@@ -58,14 +59,21 @@ class User(AbstractUser):
     REQUIRED_FIELDS = ['email', 'first_name', 'last_name', 'gender']
 
     def clean(self):
-        username = self.username.lower()
+        username = self.username.lower()  # get username
+        email = self.email  # get email
 
         with open('account/username-reserved/username.json', 'r') as usernames:
             username_reserved = load(usernames)
 
         for item in username_reserved:
-            if username == item:
+            if username == item:  # Checking the username that is not in the reserved usernames
                 raise ValidationError('Sorry, this username is not allowed')
+
+        verify_email = validate_email(email, check_mx=True, verify=True)
+        if verify_email == None or verify_email == False:  # check valid email
+            raise ValidationError(
+                'Your email is not valid. Please use a valid email'
+            )
 
     def __str__(self):
         return self.username
