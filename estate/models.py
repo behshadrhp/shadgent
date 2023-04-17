bca00525ccd96d87543eb439e0f1d8d231af924b
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-04-17 16:51:02 +0330

    update: Estate model

--- estate/models.py
+++ estate/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.conf import settings
 from django.db import models
 
@@ -17,6 +18,10 @@ class Estate(models.Model):
     OFFICIAL = 'Official'
     RESIDENTIAL = 'Residential'
 
+    # type of estate request
+    SALE = 'Sale'
+    RENT = 'Rent'
+
     # Selection parameters of estate status
     STATUE_ESTATE = [
         (VILLA, 'Villa'),
@@ -31,6 +36,12 @@ class Estate(models.Model):
         (RESIDENTIAL, 'Residential'),
     ]
 
+    # The type of customer request for the estate
+    TYPE_OF_ESTATE_REQUEST = [
+        (SALE, 'Sale'),
+        (RENT, 'Rent'),
+    ]
+
     User = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE
@@ -41,7 +52,22 @@ class Estate(models.Model):
     type_of_property_estate = models.CharField(
         max_length=12, choices=TYPE_OF_PROPERTY_ESTATE, default=COMMERCIAL
     )
+    type_of_estate_request = models.CharField(
+        max_length=5, choices=TYPE_OF_ESTATE_REQUEST, default=RENT
+    )
     meterage = models.PositiveIntegerField()
     number_room = models.PositiveIntegerField()
+    number_of_floors = models.PositiveIntegerField(
+        default=1, 
+        validators=[
+            MinValueValidator(1), 
+            MaxValueValidator(20)
+        ]
+    )
     year_made = models.DateField()
     address = models.CharField(max_length=50)
+
+    # property checklist
+    elevator = models.BooleanField(default=False)
+    parking = models.BooleanField(default=False)
+    yard = models.BooleanField(default=False)
