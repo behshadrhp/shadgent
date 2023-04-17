574440e7ea34aff4d40f49947f81a4da4c2b0f53
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-04-17 06:51:44 +0330

    change-databse-to: slqlite & update gitignore

--- core/settings.py
+++ core/settings.py
@@ -76,13 +76,9 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'shadogent',
-        'USER': 'root',
-        'PASSWORD': 'toor',
-        'HOST': 'localhost',
-        'PORT': '3306'
+     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
