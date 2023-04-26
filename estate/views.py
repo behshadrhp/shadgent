7157a18f2fcf48dcd2581dc6b702e368e31263a1
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-04-26 17:54:45 +0330

    create: EstateViewSet in estate app

--- estate/views.py
+++ estate/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from estate import models, serializers
 
-# Create your views here.
+
+class EstateViewSet(ModelViewSet):
+    '''EstateViewSet is a kind of class for presenting the real estate model,
+      as an api with rest-framework.'''
+
+    queryset = models.Estate.objects.all()
+    serializer_class = serializers.EstateSerializer
+    http_method_names = ['get', 'post', 'put', 'patch', 'delete']
+    lookup_field = 'slug'
+
+    def get_serializer_context(self):
+        owner = self.request.user
+        return {'owner': owner}
