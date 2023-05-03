17e837ce9100fb848d03bb03e2a1c8b2985f9d57
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-05-03 07:03:27 +0330

    update: api path at estate app

--- estate/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from estate import views
-
-
-router = DefaultRouter()
-router.register('', views.EstateViewSet, basename='estate')
-
-urlpatterns = router.urls
