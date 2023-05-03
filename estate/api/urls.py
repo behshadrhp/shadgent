f3d734a9a1fa0016698de757db23d4988f84e8fc
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-05-03 07:23:23 +0330

    make: router in urls at estate app

--- estate/api/urls.py
+++ estate/api/urls.py
@@ -1,3 +1,9 @@
-urlpatterns = [
-    
-]
+from estate.api import views
+
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('estate', views.EstateViewSet, basename='estate')
+
+
+urlpatterns = router.urls
